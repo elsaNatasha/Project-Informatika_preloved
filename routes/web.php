31676b66f39f915279160ab346f8003b29dc1bcb
livianasmgr <livianasumigar710@gmail.com>
2024-11-24 23:49:09 +0700

    benerin routes

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,16 @@ Route::get('/product/{id}/edit', [ProductsController::class, 'edit'])->name('pro
 Route::put('/product/{id}', [ProductsController::class, 'update'])->name('product.update');
 Route::get('/products', [ProductsController::class, 'showForBuyers'])->name('products.buyers');
 
+//=======
+
+// Route::middleware(['auth'])->group(function () {
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+// });
+//Profile Penjual
+Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::get('/profile/financial-report', [ProfileController::class, 'financialReport'])->name('financial.report');
+Route::get('/profile/product/create', [ProfileController::class, 'createProduct'])->name('product.create');
+Route::post('/logout', [ProfileController::class, 'logout'])->name('logout');
+//Edit Profile
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+//>>>>>>> Stashed changes
