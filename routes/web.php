beea4f5d0e7082cd13910425ca10ee40b0b139fa
mariatria26 <mariatria26@gmail.com>
2024-11-25 21:22:55 +0700

    meyimpan perubahan pada routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+
 use App\Http\Controllers\Auth\RegisterController;
-=======
+
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductsController;
 
->>>>>>> 8902ebe0c2439c6d25fc1638a0470dd398032eba
+ 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,7 +21,7 @@ use App\Http\Controllers\ProductsController;
 
 /*Route::get('/', function () {
     return view('welcome');
-<<<<<<< HEAD
+
 });*/
 
 
@@ -30,8 +30,8 @@ use App\Http\Controllers\ProductsController;
 
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
-=======
-});
+
+
 
 Route::get('login/', function () {
     return view('login');
@@ -45,4 +45,4 @@ Route::resource('/category',CategoryController::class);
 Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
 Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
 
->>>>>>> 8902ebe0c2439c6d25fc1638a0470dd398032eba
+
