d00202344627945b3bc5f82d1f41a016946e9858
livianasmgr <livianasumigar710@gmail.com>
2024-11-26 00:03:58 +0700

    halaman mix and match

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MixMatchController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
 
@@ -20,7 +21,6 @@ use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 
 
@@ -46,17 +46,15 @@ Route::resource('/category',CategoryController::class);
 Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
 Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
 
-//<<<<<<< Updated upstream
 Route::resource('/product',ProductsController::class);
 Route::get('/product/{id}/edit', [ProductsController::class, 'edit'])->name('product.edit');
 Route::put('/product/{id}', [ProductsController::class, 'update'])->name('product.update');
 Route::get('/products', [ProductsController::class, 'showForBuyers'])->name('products.buyers');
 
-//=======
 
-// Route::middleware(['auth'])->group(function () {
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
-// });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+});
 //Profile Penjual
 Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::get('/profile/financial-report', [ProfileController::class, 'financialReport'])->name('financial.report');
@@ -64,4 +62,8 @@ Route::get('/profile/product/create', [ProfileController::class, 'createProduct'
 Route::post('/logout', [ProfileController::class, 'logout'])->name('logout');
 //Edit Profile
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-//>>>>>>> Stashed changes
+Route::get('/editprofile', [ProfileController::class, 'editProfile'])->name('editprofile');
+
+
+// Mix and Match
+Route::get('/mix-match', [MixMatchController::class, 'index'])->name('');
